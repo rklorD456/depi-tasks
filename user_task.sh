0d578ec87fd4acd125730a30e3d4c0e384f241f0
rklorD456 <wermasd192@gmail.com>
2025-07-29 18:55:04 +0300

    update bash

--- user_task.sh
+++ user_task.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
+shopt -s expand_aliases
 
+echo "--- Starting Script ---"
+echo
+
+
+echo "Adding and removing user 'mahmoud'..."
 sudo adduser mahmoud
 
 
 sudo deluser --remove-home mahmoud
 
+echo "Creating and removing a temporary directory..."
 mkdir working-area
 rmdir working-area
+
+echo "-> 'working-area' created and removed."
+echo
+
 
 echo "hello my new user" >> file.txt
 grep "hello" file.txt
 
 cat file.txt
 
+echo "Changing file permissions..."
 chmod 700 file.txt
 chmod u-x file.txt
+echo "-> Permissions set."
+echo
 
 alias ll='ls -la'
 ll
 unalias ll
 
-date
-pwd
+echo "Displaying current date and path..."
+echo "Date: $(date)"
+echo "Path: $(pwd)"
+echo
+
 
 history 2
 
@@ -33,4 +50,4 @@ mv file.txt newone1/
 rm -r newone newone1
 
 ls
-echo "✅ Script completed!"
+echo "Script completed!"
